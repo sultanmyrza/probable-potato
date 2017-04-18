701dfd6d54b4492f74c64fcfd5e64532d15fc4be
sultanmyrza <sultanmyrza@gmail.com>
2017-04-19 08:25:48 +0900

    import sys

--- project/wsgi.py
+++ project/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
+from django.conf import settings
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
 application = get_wsgi_application()
+
+
+if not settings.DEBUG:
+    import sys
+    sys.path.append()
